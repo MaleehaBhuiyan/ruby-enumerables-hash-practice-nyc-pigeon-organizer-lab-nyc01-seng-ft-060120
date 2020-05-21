4f909ec55222772a1dcc7a7c411d24814975be12
Maleeha Bhuiyan <maleeha.bhuiyan@baruchmail.cuny.edu>
2020-05-21 02:43:29 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,6 +1,6 @@
 require 'pry'
 def nyc_pigeon_organizer(data)
-  data.each_with_object({}) do |(key, value), final_array|
+  final_results = data.each_with_object({}) do |(key, value), final_array|
     #binding.pry
     value.each do |inner_key, names|
       names.each do |name|
@@ -11,7 +11,7 @@ def nyc_pigeon_organizer(data)
       if !final_array[name][key]
         !final_array[name][key]= []
     end
-    final_array[name][key]
+    final_array[name][key].push(inner_key)
   end
   binding.pry
 end
