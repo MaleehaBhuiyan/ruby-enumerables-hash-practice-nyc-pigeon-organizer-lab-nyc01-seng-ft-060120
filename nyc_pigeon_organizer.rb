a6e63089819fb549d2be4a78f068012e48a4121b
Maleeha Bhuiyan <maleeha.bhuiyan@baruchmail.cuny.edu>
2020-05-21 02:42:35 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -8,8 +8,10 @@ def nyc_pigeon_organizer(data)
         if !final_array[name]
           final_array[name] = {}
       end
+      if !final_array[name][key]
+        !final_array[name][key]= []
     end
-    final_array
+    final_array[name][key]
   end
   binding.pry
 end
